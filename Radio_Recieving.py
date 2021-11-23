0d989719f276b438f5cc73a44e915978c6aaf2f8
Adrian Gamarra <adriangamarra2018@gmail.com>
2021-11-23 13:57:27 -0800

    Added comments. Hopefully more readable

--- Radio_Recieving.py
+++ Radio_Recieving.py
@@ -4,6 +4,7 @@ import digitalio
 
 import adafruit_rfm9x
 
+#####  Initialization  #######
 ANTENNA_ATTACHED = True
 
 RADIO_FREQ_MHZ = 433.0
@@ -16,25 +17,28 @@ LED.direction = digitalio.Direction.OUTPUT
 
 radio = adafruit_rfm9x.RFM9x(spi, cs, reset, 433.0)
 
-#rfm9x.ack_retries = 2
-#rfm9x.ack_wait = 0.5
-radio.enable_crc = True  # idk what this does, but we need it :D
+# rfm9x.ack_retries = 2
+# rfm9x.ack_wait = 0.5
+radio.enable_crc = True
 radio.node = 255  # accept all
 radio.destination = 255  # send to all
+####  Initialization End #####
 
+# For ID tracking
 count = 0
-
 seendID = bytearray(256)
 
-finalNode = b'\xab'
-origNode = b'\xaa'
-noSatAck = b'\x00' #  This could be changed with a flag, learn how to do this :). Maybe look at send_with_ack in library to see how to edit flags
-yesSatAck = b'\xaa'
+finalNode = b'\xab' # Destination radio
+origNode = b'\xaa'# Current radio. Changes depending on the radio
 
+# Used for acknowledging a received signal
+noSatAck = b'\x00'
+yesSatAck = b'\xaa'
 seen = False
 
 while True:
 
+    # Wait to receive signal
     response = radio.receive(keep_listening=True, with_header=True, timeout=None)
 
     if response is not None:
@@ -44,16 +48,20 @@ while True:
         #print("Received (raw bytes): {0}".format(response))
 
         for ID in seendID:
-            if response[2:3] == ID: # we have seen this message
-                #if response[2] == ID: should work, check if it does. above works for sure
+            if response[2:3] == ID:  # we have seen this message
                 seen = True
                 print("already seen this message")
 
         if not seen:
             if response[4:5] == origNode:
                 print("meant for this!")
+
+                # Build new message to send back
                 finalMessage = finalNode + origNode + yesSatAck
-                count = response[2]
+                count = response[2]  # send back with same ID
                 print("Sending ack back")
                 print(count)
-                radio.send(finalMessage, identifier = count, destination=255, keep_listening=True)
+
+                radio.send(finalMessage, identifier=count, destination=255, keep_listening=True)
+
+
--- Radio_Sending.py
+++ Radio_Sending.py
@@ -5,6 +5,7 @@ import time
 
 import adafruit_rfm9x
 
+#####  Initialization  #######
 ANTENNA_ATTACHED = True
 
 RADIO_FREQ_MHZ = 433.0
@@ -17,23 +18,28 @@ LED.direction = digitalio.Direction.OUTPUT
 
 radio = adafruit_rfm9x.RFM9x(spi, cs, reset, 433.0)
 
-#rfm9x.ack_retries = 2
-#rfm9x.ack_wait = 0.5
-radio.enable_crc = True  # idk what this does
-radio.node = 255  # accept all(?) Idk if we need to specify or not
+# rfm9x.ack_retries = 2
+# rfm9x.ack_wait = 0.5
+radio.enable_crc = True
+radio.node = 255  # accept all
 radio.destination = 255  # send to all
+####  Initialization End #####
 
+
+# For ID tracking
 count = 0
-
 seendID = bytearray(256)
 
-# #Comment out when testting as it will not run without input
 #  finalNode = input("Node destination: ")
-finalNode = b'\xaa'
-origNode = b'\xab'
-noSatAck = b'\x00' #  This could be changed with a flag, learn how to do this :). Maybe look at send_with_ack in library to see how to edit flags
+finalNode = b'\xaa'  # Destination radio
+origNode = b'\xab'  # Current radio. Changes depending on the radio
+
+# This could be changed with a flag
+# Used for acknowledging a received signal
+noSatAck = b'\x00'
 yesSatAck = b'\xaa'
 
+# Building message to send out
 #  finalMessage = input("What do you want to tell the node?: ")
 finalMessage = finalNode
 finalMessage += origNode
@@ -44,28 +50,25 @@ print(finalMessage)
 
 while True:
 
-    #  receving radio will switch from listening to all to accepting just ack
-    #  the radios will have the window open for 100 seconds before accepting
-    #  all again
     print("Sending message out to all nodes")
     count += 1
     radio.send(finalMessage, identifier=count, destination=255, keep_listening=True)
 
-    # adding ID to seen, mainly to check for ack
-    seendID[count] = count #  play around with bytearray
+    # adding ID to seen
+    seendID[count] = count
 
+    # wait for response for 10 seconds
     response = radio.receive(keep_listening=True, with_header=True, timeout=10)
 
     if response is not None:
-        #print("Received (raw header):", [hex(x) for x in response[0:4]])
+        # print("Received (raw header):", [hex(x) for x in response[0:4]])
         print("Received (raw payload): {0}".format(response[4:]))
-        #print("Received RSSI: {0}".format(radio.last_rssi))
+        # print("Received RSSI: {0}".format(radio.last_rssi))
 
         for ID in seendID:
-            if response[2] == ID: # we have seen this message
-                if response[6:7] == yesSatAck: # its our ack!
+            if response[2] == ID:  # we have seen this message. We use single numbers to access the int value of the byte
+                if response[6:7] == yesSatAck:  # its our ack! We use number:number to access the string of the byte
                     print("signal was acknowledged by radio")
-
 
     else:
         print("No ack in 10 seconds")
