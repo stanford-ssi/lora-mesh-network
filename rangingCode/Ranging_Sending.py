f54207c10588feaa835d9e3003bef6a824cd0e9d
snadee001 <67486920+snadee001@users.noreply.github.com>
2021-12-22 00:54:08 -0800

    Update Ranging_Sending.py
    First draft

--- rangingCode/Ranging_Sending.py
+++ rangingCode/Ranging_Sending.py
@@ -25,3 +25,54 @@ radio.destination = 255  # send to all
 # 5-23, 13 is default. Power of radio signal
 radio.tx_power = 13
 ####  Initialization End #####
+
+################################################
+
+# For ID tracking
+count = 0
+seendID = bytearray(256)
+
+#  finalNode = input("Node destination: ")
+finalNode = b'\xcc'  # Destination radio
+origNode = b'\xaa'  # Current radio. Changes depending on the radio
+
+# Used for acknowledging a received signal
+noSatAck = b'\x00'
+yesSatAck = origNode
+
+#Building message to send out
+finalMessage = finalNode
+finalMessage += origNode
+print(finalMessage)
+
+
+#transmits a signal
+while True:
+    start = time.perf_counter()
+    print("Start Ranging")
+
+    radio.send(finalMessage, identifier=count, keep_listening=True)
+    listenAgain = True
+
+    while listenAgain:
+        response = radio.receive(keep_listening=True, with_header=True, timeout=3)
+        listenAgain=False
+
+        if response is not None:
+            LED.value = True
+            print("Received signal!")
+
+            for ID in seendID:
+                if response[2] == ID:  # we have seen this message. We use single numbers to access the int value of the byte
+                    if response[6:7] != noSatAck: # its our ack! We use number:number to access the string of the byte
+                        print("signal was acknowledged by radio directly")
+                        listenAgain = False
+                    else:
+                        listenAgain = False
+        else:
+            LED.value = False
+            print("No signal in 3 seconds")
+
+#receives a repsonse an uses a timer to measure how long the exchange took
+stop = time.perf_counter()
+print(start - stop)
