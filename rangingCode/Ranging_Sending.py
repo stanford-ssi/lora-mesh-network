bde7536388fc3cc636002c55521e9b389c3a2dbe
Adrian Gamarra <adriangamarra2018@gmail.com>
2021-12-21 19:35:29 -0800

    Start of Ranging Code

---
+++ rangingCode/Ranging_Sending.py
@@ -0,0 +1,27 @@
+import board
+import busio
+import digitalio
+
+import adafruit_rfm9x
+
+#####  Initialization  #######
+ANTENNA_ATTACHED = True
+
+RADIO_FREQ_MHZ = 433.0
+cs = digitalio.DigitalInOut(board.D5)
+reset = digitalio.DigitalInOut(board.D6)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+LED = digitalio.DigitalInOut(board.D13)
+LED.direction = digitalio.Direction.OUTPUT
+
+radio = adafruit_rfm9x.RFM9x(spi, cs, reset, 433.0)
+
+# rfm9x.ack_retries = 2
+# rfm9x.ack_wait = 0.5
+radio.enable_crc = True
+radio.node = 255  # accept all
+radio.destination = 255  # send to all
+# 5-23, 13 is default. Power of radio signal
+radio.tx_power = 13
+####  Initialization End #####
