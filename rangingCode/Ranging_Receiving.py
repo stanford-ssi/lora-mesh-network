f996b8dad1a8eb2bb36394951086e17f1af1455e
snadee001 <67486920+snadee001@users.noreply.github.com>
2021-12-22 00:52:09 -0800

    Update Ranging_Receiving.py
    First draft

--- rangingCode/Ranging_Receiving.py
+++ rangingCode/Ranging_Receiving.py
@@ -25,3 +25,49 @@ radio.destination = 255  # send to all
 # 5-23, 13 is default. Power of radio signal
 radio.tx_power = 13
 ####  Initialization End #####
+
+###################################################
+
+# For ID tracking
+seendID = bytearray(200)
+loop = 0
+
+finalNode = b'\xff' # Destination radio
+origNode = b'\xbb'# Current radio. CHANGE depending on the radio
+
+# Used for acknowledging a received signal
+noSatAck = b'\x00'
+yesSatAck = origNode
+
+
+while True:
+    response = radio.receive(keep_listening=True, with_header=True, timeout=1)
+    seen=False
+    
+    if response is not None:
+        messageID = response[2]
+        
+        for ID in seendID:
+            if messageID == ID:
+                seen = True
+                print("already seen this message")
+                
+    
+    if not seen:
+        if response[4:5] == origNode:
+            print("Radio 2 recieved response")
+            finalMessage = response[5:6] + response[4:5] + yesSatAck + response[7:8]  
+            radio.send(finalMessage, identifier=messageID, keep_listening=True)
+            
+        else:
+            print("Not meant for this")
+            print("Re-relaying the message")
+
+            #build new message to send back
+            finalMessage = response[4:7] + yesRelay
+            print(finalMessage)
+
+            radio.send(finalMessage, identifier=messageID, keep_listening=True)
+            
+
+
